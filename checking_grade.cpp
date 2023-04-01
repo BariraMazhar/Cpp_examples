0cddc84c00560ad49dd54db6b2c4f950c09d4b52
Barira-Mazhar <bmmcpk@gmail.com>
2023-04-01 09:39:45 -0700

    Cpp Examples

---
+++ checking_grade.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int marks;
+    cout<<"Enter marks: "<<endl;
+    cin>> marks;
+    if (marks>=90)
+    {
+        cout<<" A+"<<endl;
+    }
+    else if (marks>=80)
+    {
+        cout<<" B+"<<endl;
+    }
+    else
+    {
+        cout<<" C "<<endl;
+    }
+    return 0;
+    
+}
